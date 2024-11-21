efcfe633338fd625e906af2ad7ab7ef8526f982c
Naseer Ahmad <naseerahma82@gmail.com>
2024-11-21 06:57:16 +0500

    sys.py has been added to the repo
    changes has been made in the 1st print statment we have used commit rebase -i to squash the commit

---
+++ sys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+name = sys.argv[0]
+print(f"welcome dear  {name}")
+
+
+
+for i in range(10):
+	print(i)
+
+print("this line is added to check the conflict in the two branches")
+
+
